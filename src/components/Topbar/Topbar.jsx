a9f5ab034c3d425e8cb486f884cf5402f0151847
Ramees <rameesc985@gmail.com>
2025-06-23 11:03:22 +0530

    topbar component created

---
+++ src/components/Topbar/Topbar.jsx
@@ -0,0 +1,52 @@
+import React from "react";
+
+import {
+  FaMapMarkerAlt,
+  FaEnvelope,
+  FaPhoneAlt,
+  FaFacebookF,
+  FaTwitter,
+  FaLinkedinIn,
+} from "react-icons/fa";
+
+
+function Topbar() {
+  return (
+    <div className="bg-black text-white hidden lg:flex animate-fade-in">
+      <div className="container py-3">
+        <div className="flex items-center justify-center">
+          
+          <div className="ml-auto flex justify-center items-center">
+            <div className="ml-10 flex items-center justify-center gap-2 ">
+              <FaMapMarkerAlt className="text-lg" />
+              <p className="text-sm">123 Street, New York, USA</p>
+            </div>
+            <div className="ml-10 flex items-center justify-center gap-2">
+              <FaEnvelope className="text-lg" />
+
+              <p className="text-sm">info@example.com</p>
+            </div>
+            <div className="ml-10 flex items-center justify-center gap-2">
+              <FaPhoneAlt className="text-lg" />
+
+              <p className="text-sm">+012 345 67890</p>
+            </div>
+            <div className="ml-10 flex gap-2">
+              <a className="bg-white text-black p-1" href="">
+                <FaFacebookF />
+              </a>
+              <a className="bg-white text-black p-1" href="">
+                <FaTwitter />
+              </a>
+              <a className="bg-white text-black p-1" href="">
+                <FaLinkedinIn />
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Topbar;
