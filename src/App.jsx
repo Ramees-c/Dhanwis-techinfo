39b70e63406f93e3e48d57717ba3eb132581cd0b
Ramees <rameesc985@gmail.com>
2025-06-27 11:28:26 +0530

    client logo and portfolio card images changed

--- src/App.jsx
+++ src/App.jsx
@@ -1,53 +1,54 @@
 import React from "react";
 import Home from "./pages/Home/Home";
 
+import PortfolioImg1 from "./assets/images/portfolioImages/Exora Conventions and 14 more pages - Profile 1 - Microsoft_ Edge 11-01-2024 09_08_24.png"
+
 function App() {
   const portfolioContent = [
     {
       id:1,
-      image: "http://dhanwis.com/img/portfolio/Exora%20Conventions%20and%2014%20more%20pages%20-%20Profile%201%20-%20Microsoft%E2%80%8B%20Edge%2011-01-2024%2009_08_24.png",
-      title: "title",
+      image: PortfolioImg1,
       desc: "Lorem ipsum, dolor sit amet consectetur adipisicing elit.",
     },
     {
       id:2,
-      image: "https://img.freepik.com/free-vector/flat-design-responsive-website-design_23-2149489189.jpg?ga=GA1.1.1246216932.1730456119&semt=ais_items_boosted&w=740",
+      image: PortfolioImg1,
       title: "title",
       desc: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. ",
     },
     {
       id:3,
-      image: "https://img.freepik.com/free-vector/flat-design-responsive-website-design_23-2149489189.jpg?ga=GA1.1.1246216932.1730456119&semt=ais_items_boosted&w=740",
+      image: PortfolioImg1,
       title: "title",
       desc: "Lorem ipsum, dolor sit amet consectetur adipisicing elit.",
     },
     {
       id:4,
-      image: "https://img.freepik.com/free-vector/flat-design-responsive-website-design_23-2149489189.jpg?ga=GA1.1.1246216932.1730456119&semt=ais_items_boosted&w=740",
+      image: PortfolioImg1,
       title: "title",
       desc: "Lorem ipsum, dolor sit amet consectetur adipisicing elit.",
     },
     {
       id:5,
-      image: "https://img.freepik.com/free-vector/flat-design-responsive-website-design_23-2149489189.jpg?ga=GA1.1.1246216932.1730456119&semt=ais_items_boosted&w=740",
+      image: PortfolioImg1,
       title: "title",
       desc: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. ",
     },
     {
       id:6,
-      image: "https://img.freepik.com/free-vector/flat-design-responsive-website-design_23-2149489189.jpg?ga=GA1.1.1246216932.1730456119&semt=ais_items_boosted&w=740 ",
+      image: PortfolioImg1,
       title: "title",
       desc: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. ",
     },
     {
       id:7,
-      image: "https://img.freepik.com/free-vector/flat-design-responsive-website-design_23-2149489189.jpg?ga=GA1.1.1246216932.1730456119&semt=ais_items_boosted&w=740",
+      image: PortfolioImg1,
       title: "title",
       desc: "Lorem ipsum, dolor sit amet consectetur adipisicing elit.",
     },
     {
       id:8,
-      image: "https://img.freepik.com/free-vector/flat-design-responsive-website-design_23-2149489189.jpg?ga=GA1.1.1246216932.1730456119&semt=ais_items_boosted&w=740",
+      image: PortfolioImg1,
       title: "title",
       desc: "Lorem ipsum, dolor sit amet consectetur adipisicing elit.",
     },
--- src/components/ClientLogos/ClientLogos.jsx
+++ src/components/ClientLogos/ClientLogos.jsx
@@ -1,19 +1,26 @@
 import React from "react";
 
+import logo1 from "../../assets/images/clientLogos/Ashraya Medicals - Google Chrome 09-01-2024 14_11_26.png";
+
 function ClientLogos() {
   const clients = [
-    { id: 1, name: "Client 1", logo: "http://dhanwis.com/img/clients/Ashraya%20Medicals%20-%20Google%20Chrome%2009-01-2024%2014_11_26.png" },
-    { id: 2, name: "Client 2", logo: "http://dhanwis.com/img/clients/Ashraya%20Medicals%20-%20Google%20Chrome%2009-01-2024%2014_10_23.png" },
-    { id: 3, name: "Client 3", logo: "http://dhanwis.com/img/clients/GOLDMAN%20INTERIORS%20LLP%20_%20BEST%20BUILDERS%20IN%20KERALA%20AND%20MIDDLE%20EAST%20-%20Google%20Chrome%2009-01-2024%2014_10_13.png" },
-    { id: 4, name: "Client 4", logo: "http://dhanwis.com/img/clients/Ashraya%20Medicals%20-%20Google%20Chrome%2009-01-2024%2014_11_12.png" },
-    { id: 5, name: "Client 5", logo: "http://dhanwis.com/img/clients/Ashraya%20Medicals%20-%20Google%20Chrome%2009-01-2024%2014_10_52.png" },
-    { id: 6, name: "Client 6", logo: "http://dhanwis.com/img/clients/Cinema%20News%20Agency%20_%20Exclusive%20Cinema%20Magazine%20in%20Malayalam%20and%2018%20more%20pages%20-%20Profile%201%20-%20Microsoft%E2%80%8B%20Edge%2010-01-2024%2014_21_52.png" },
+    { id: 1, name: "Client 1", logo: logo1 },
+    { id: 2, name: "Client 2", logo: logo1 },
+    { id: 3, name: "Client 3", logo: logo1 },
+    { id: 4, name: "Client 4", logo: logo1 },
+    { id: 5, name: "Client 5", logo: logo1 },
+    { id: 6, name: "Client 6", logo: logo1 },
   ];
   return (
     <section className="container py-12 ">
       <div className="container mx-auto px-4">
-        <h2 className="text-2xl lg:text-5xl font-bold text-center mb-5 font-title">Our Happy Clients</h2>
-        <p className="text-center mb-5 lg:mb-12 text-sm lg:text-lg text-para">We collaborate closely with a broad spectrum of clients from various industries and places.</p>
+        <h2 className="text-2xl lg:text-5xl font-bold text-center mb-5 font-title">
+          Our Happy Clients
+        </h2>
+        <p className="text-center mb-5 lg:mb-12 text-sm lg:text-lg text-para">
+          We collaborate closely with a broad spectrum of clients from various
+          industries and places.
+        </p>
 
         <div className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 gap-3 lg:gap-7">
           {clients.map((client) => (
